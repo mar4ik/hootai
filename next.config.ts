2e47c8bcc00f5206bc18717222d6d9d71a9cac64
mar4ik <mariam.morozova@gmail.com>
2025-05-22 22:37:48 +0400

    pachkage.json fuxes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["localhost"],
   },
+  // Suppress specific deprecation warnings
+  webpack: (config, { isServer }) => {
+    // Ignore the node-domexception deprecation warning
+    config.ignoreWarnings = [
+      { module: /node_modules\/node-domexception/ }
+    ];
+    
+    return config;
+  },
 };
 
 export default nextConfig;
