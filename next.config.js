72684062075d1b68db8f66827b9da7b4d21e5e67
mar4ik <mariam.morozova@gmail.com>
2025-05-23 19:04:22 +0400

    vercel json

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   images: {
     domains: ["localhost"],
   },
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/\\+\\+api\\+\\+/:slug*",
-        destination: "http://localhost:8080/Plone/%2B%2Bapi%2B%2B/:slug*",
-      },
-    ];
-  },
   // Suppress specific deprecation warnings
   webpack: (config, { isServer }) => {
     // Ignore the node-domexception deprecation warning
