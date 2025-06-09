158c1223f4af0d5d7898f024494ed1e056367c48
mar4ik <mariam.morozova@gmail.com>
2025-06-09 22:21:50 +0400

    hotjar script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,19 @@ export default function RootLayout({
             `
           }}
         />
+        {/* Hotjar Tracking Code */}
+        <Script id="hotjar" strategy="afterInteractive">
+            {`
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6429905,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `}
+          </Script>
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-1NXXGW8Z77"
